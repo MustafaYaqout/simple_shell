ed660c6e2d8935f634214bdffb3712b77478675a
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-11-15 21:27:27 +0200

    ahmed

--- handleError.c
+++ handleError.c
@@ -3,8 +3,10 @@
 /**
  * HandleError - Displays an error message, frees allocated memory, and exits.
  *
- * @param message - Pointer to a constant character (string) representing the error message.
- * @param arguments - Pointer to a pointer to a character, assumed to be an array of strings (or an array of pointers to characters).
+ * @message: Pointer to a constant character (string)
+ * representing the error message.
+ * @arguments:Pointer to a pointer to a character, assumed to be an arra
+ * of strings (or an array of pointers to characters).
  */
 
 void HandleError(const char *message, char **arguments)
--- readUserInput.c
+++ readUserInput.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * Read a line of input from the user.
+ * readUserInput - Read a line of input from the user.
  *
  * This function reads a line of input from the standard input (stdin).
  * It dynamically allocates memory to store the input string, and it
  * adjusts the string to remove the newline character at the end, if present.
  *
  * @return A dynamically allocated string containing the user's input.
- *         The caller is responsible for freeing the allocated memory.
  */
 char *readUserInput(void)
 {
