124c2f3d9c187c7957812f9967180cbc1334f8ca
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-11-15 22:05:33 +0200

    ahmed

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - Entry point of the shell program.
  *
@@ -11,7 +10,6 @@
  *
  * Return: 0 on successful execution, otherwise appropriate error code.
  */
-
 int main(int argc, char *argv[], char *envp[])
 {
 	char *lineptr = NULL;
@@ -19,35 +17,27 @@ int main(int argc, char *argv[], char *envp[])
 	ssize_t nchars_read;
 	char **arguments = NULL;
 	int i;
-
 	(void)argc;
 	(void)argv;
 	(void)envp;
-
 	while (1)
 	{
 		nchars_read = getline(&lineptr, &n, stdin);
-
 		if (nchars_read == -1)
 		{
 			free(lineptr);
 			return (0);
 		}
-
 		if (nchars_read > 0 && lineptr[nchars_read - 1] == '\n')
 		{
 			lineptr[nchars_read - 1] = '\0';
 		}
-
 		arguments = parse_cmd(lineptr);
-
 		if (arguments == NULL)
 		{
-
 			free(lineptr);
 			return (1);
 		}
-
 		ExecuteCommand(arguments);
 		for (i = 0; arguments[i] != NULL; i++)
 		{
@@ -55,7 +45,6 @@ int main(int argc, char *argv[], char *envp[])
 		}
 		free(arguments);
 	}
-
 	free(lineptr);
 	return (0);
 }
