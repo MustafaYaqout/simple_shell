17353e00a3771889a47079a71bd75c4b5b53eb43
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-11-15 20:54:25 +0200

    ahmed

--- handleError.c
+++ handleError.c
@@ -3,8 +3,8 @@
 /**
  * HandleError - Displays an error message, frees allocated memory, and exits.
  *
- * @param message - Error message to display.
- * @param arguments - Memory to free.
+ * @param message - Pointer to a constant character (string) representing the error message.
+ * @param arguments - Pointer to a pointer to a character, assumed to be an array of strings (or an array of pointers to characters).
  */
 
 void HandleError(const char *message, char **arguments)
